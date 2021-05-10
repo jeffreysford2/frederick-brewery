4106499ee741d961df23e2ac13ec6362043ab793
jeffreysford2 <jeffreysford2@gmail.com>
2021-05-10 16:48:28 -0400

    Corrected cors issue

--- src/App.js
+++ src/App.js
@@ -1,158 +1,152 @@
 import Brewery from './components/Brewery'
+import axios from 'axios'
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+const API_KEY = process.env.REACT_APP_API_KEY
 
 
 
 
-const breweries = [
-  {
-    id: 1,
-    name: 'Idiom Brewing Co.',
-    address: '340 E Patrick St #104, Frederick, MD 21701',
-    hours: [
-      'Sunday: 12:00pm - 10:00pm',
-      'Monday: 12:00pm - 10:00pm',
-      'Tuesday: 12:00pm - 10:00pm',
-      'Wednesday: 12:00pm - 10:00pm',
-      'Thursday: 12:00pm - 10:00pm',
-      'Friday: 12:00pm - 10:00pm',
-      'Saturday: 12:00pm - 10:00pm'
-    ],
-    website: 'https://www.idiombrewing.com/',
-    instagram: 'https://www.instagram.com/idiombrewingco/',
-    facebook: 'https://www.facebook.com/idiombrewingco',
-    phoneNumber: '(240) 578-4152'
-  },
-  {
-    id: 2,
-    name: 'Attaboy Beer',
-    address: '340 E Patrick St #104, Frederick, MD 21701',
-    hours: [
-      'Sunday: 12:00pm - 10:00pm',
-      'Monday: 12:00pm - 10:00pm',
-      'Tuesday: 12:00pm - 10:00pm',
-      'Wednesday: 12:00pm - 10:00pm',
-      'Thursday: 12:00pm - 10:00pm',
-      'Friday: 12:00pm - 10:00pm',
-      'Saturday: 12:00pm - 10:00pm'
-    ],
-    website: 'https://www.idiombrewing.com/',
-    instagram: 'https://www.instagram.com/idiombrewingco/',
-    facebook: 'https://www.facebook.com/idiombrewingco',
-    phoneNumber: '(240) 578-4152'
-  }, {
-    id: 3,
-    name: 'Rockwell Brewery',
-    address: '340 E Patrick St #104, Frederick, MD 21701',
-    hours: [
-      'Sunday: 12:00pm - 10:00pm',
-      'Monday: 12:00pm - 10:00pm',
-      'Tuesday: 12:00pm - 10:00pm',
-      'Wednesday: 12:00pm - 10:00pm',
-      'Thursday: 12:00pm - 10:00pm',
-      'Friday: 12:00pm - 10:00pm',
-      'Saturday: 12:00pm - 10:00pm'
-    ],
-    website: 'https://www.idiombrewing.com/',
-    instagram: 'https://www.instagram.com/idiombrewingco/',
-    facebook: 'https://www.facebook.com/idiombrewingco',
-    phoneNumber: '(240) 578-4152'
-  },
-  {
-    id: 4,
-    name: 'Moncoacy Brewing Co.',
-    address: '340 E Patrick St #104, Frederick, MD 21701',
-    hours: [
-      'Sunday: 12:00pm - 10:00pm',
-      'Monday: 12:00pm - 10:00pm',
-      'Tuesday: 12:00pm - 10:00pm',
-      'Wednesday: 12:00pm - 10:00pm',
-      'Thursday: 12:00pm - 10:00pm',
-      'Friday: 12:00pm - 10:00pm',
-      'Saturday: 12:00pm - 10:00pm'
-    ],
-    website: 'https://www.idiombrewing.com/',
-    instagram: 'https://www.instagram.com/idiombrewingco/',
-    facebook: 'https://www.facebook.com/idiombrewingco',
-    phoneNumber: '(240) 578-4152'
-  }, {
-    id: 5,
-    name: 'Olde Mother Brewing Co.',
-    address: '340 E Patrick St #104, Frederick, MD 21701',
-    hours: [
-      'Sunday: 12:00pm - 10:00pm',
-      'Monday: 12:00pm - 10:00pm',
-      'Tuesday: 12:00pm - 10:00pm',
-      'Wednesday: 12:00pm - 10:00pm',
-      'Thursday: 12:00pm - 10:00pm',
-      'Friday: 12:00pm - 10:00pm',
-      'Saturday: 12:00pm - 10:00pm'
-    ],
-    website: 'https://www.idiombrewing.com/',
-    instagram: 'https://www.instagram.com/idiombrewingco/',
-    facebook: 'https://www.facebook.com/idiombrewingco',
-    phoneNumber: '(240) 578-4152'
-  },
-  {
-    id: 6,
-    name: 'Flying Dog Brewery',
-    address: '340 E Patrick St #104, Frederick, MD 21701',
-    hours: [
-      'Sunday: 12:00pm - 10:00pm',
-      'Monday: 12:00pm - 10:00pm',
-      'Tuesday: 12:00pm - 10:00pm',
-      'Wednesday: 12:00pm - 10:00pm',
-      'Thursday: 12:00pm - 10:00pm',
-      'Friday: 12:00pm - 10:00pm',
-      'Saturday: 12:00pm - 10:00pm'
-    ],
-    website: 'https://www.idiombrewing.com/',
-    instagram: 'https://www.instagram.com/idiombrewingco/',
-    facebook: 'https://www.facebook.com/idiombrewingco',
-    phoneNumber: '(240) 578-4152'
-  },
-  {
-    id: 7,
-    name: 'Smoketown Creekside',
-    address: '340 E Patrick St #104, Frederick, MD 21701',
-    hours: [
-      'Sunday: 12:00pm - 10:00pm',
-      'Monday: 12:00pm - 10:00pm',
-      'Tuesday: 12:00pm - 10:00pm',
-      'Wednesday: 12:00pm - 10:00pm',
-      'Thursday: 12:00pm - 10:00pm',
-      'Friday: 12:00pm - 10:00pm',
-      'Saturday: 12:00pm - 10:00pm'
-    ],
-    website: 'https://www.idiombrewing.com/',
-    instagram: 'https://www.instagram.com/idiombrewingco/',
-    facebook: 'https://www.facebook.com/idiombrewingco',
-    phoneNumber: '(240) 578-4152'
-  },
-  {
-    id: 8,
-    name: 'Steinhardt Brewing Co.',
-    address: '340 E Patrick St #104, Frederick, MD 21701',
-    hours: [
-      'Sunday: 12:00pm - 10:00pm',
-      'Monday: 12:00pm - 10:00pm',
-      'Tuesday: 12:00pm - 10:00pm',
-      'Wednesday: 12:00pm - 10:00pm',
-      'Thursday: 12:00pm - 10:00pm',
-      'Friday: 12:00pm - 10:00pm',
-      'Saturday: 12:00pm - 10:00pm'
-    ],
-    website: 'https://www.idiombrewing.com/',
-    instagram: 'https://www.instagram.com/idiombrewingco/',
-    facebook: 'https://www.facebook.com/idiombrewingco',
-    phoneNumber: '(240) 578-4152'
-  }
-]
+// const breweries = [
+//   {
+//     id: 1,
+//     name: 'Idiom Brewing Co.',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   },
+//   {
+//     id: 2,
+//     name: 'Attaboy Beer',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   }, {
+//     id: 3,
+//     name: 'Rockwell Brewery',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   },
+//   {
+//     id: 4,
+//     name: 'Moncoacy Brewing Co.',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   }, {
+//     id: 5,
+//     name: 'Olde Mother Brewing Co.',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   },
+//   {
+//     id: 6,
+//     name: 'Flying Dog Brewery',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   },
+//   {
+//     id: 7,
+//     name: 'Smoketown Creekside',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   },
+//   {
+//     id: 8,
+//     name: 'Steinhardt Brewing Co.',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   },
+//   {
+//     id: 9,
+//     name: 'Jug Bridge Brewery',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   },
+//   {
+//     id: 10,
+//     name: 'Midnight Run Brewing',
+//     address: '340 E Patrick St #104, Frederick, MD 21701',
+//     website: 'https://www.idiombrewing.com/',
+//     instagram: 'https://www.instagram.com/idiombrewingco/',
+//     facebook: 'https://www.facebook.com/idiombrewingco',
+//     phoneNumber: '(240) 578-4152'
+//   }
+// ]
 
 function App() {
-  // const [brewery, setBrewery] = useState([])
+  const [breweries, setBreweries] = useState([]);
   const [search, setSearch] = useState('')
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    axios
+      .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.4143,-77.4105&radius=20000&keyword=brewery&key=${API_KEY}`)
+      .then(res => {
+        setBreweries(res.data.results);
+        console.log('BREWERIES:', breweries)
+        //console.log(breweries[2].name)
+      })
+      .catch(error => console.log(error.response));
+  }, []);
+
+  //       },
+  //       // Note: it's important to handle errors here
+  //       // instead of a catch() block so that we don't swallow
+  //       // exceptions from actual bugs in components.
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         setError(error);
+  //       }
+  //     )
+  // }, [])
+
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // } else if (!isLoaded) {
+  //   return <div>Loading...</div>;
+  // } else {
+  //   return (
+  //     <ul>
+  //       {items.map(item => (
+  //         <li key={item.id}>
+  //           {item.name} {item.price}
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+
+
+
+
+  // const [brewery, setBrewery] = useState([])
+
 
 
 
@@ -160,7 +154,7 @@ function App() {
   const handleChange = (e) => {
     setSearch(e.target.value)
   }
-
+  console.log('breweries:', breweries)
   const filteredBreweries = breweries.filter(brewery => (
     brewery.name.toLowerCase().includes(search.toLowerCase())
   ))
@@ -180,12 +174,16 @@ function App() {
         filteredBreweries.map(brewery => {
           return (
             <Brewery
-              key={brewery.id}
-              name={brewery.name}
-              address={brewery.address}
+              key={brewery.place_id}
+              name={brewery.name.toUpperCase()}
+              address={brewery.vicinity}
+              rating={brewery.rating}
+              user_ratings_total={brewery.user_ratings_total}
+              open_now={brewery.opening_hours}
               instagram={brewery.instagram}
+              facebook={brewery.facebook}
+              website={brewery.website}
               phoneNumber={brewery.phoneNumber}
-              hours={brewery.hours}
 
             // open={true or false}
             />
--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -2,10 +2,12 @@
 import './Brewery.css'
 const Brewery = (props) => {
 
-
-    const allHours = props.hours;
-    const listHours = allHours.map((hour) => <li key={hour}>{hour}</li>);
-    console.log('allhours:', listHours)
+    const isOpen = () => {
+        if (props.open_now) {
+            return props.open_now.open_now
+        }
+        return null
+    }
 
     return (
         <div className="brewery-container">
@@ -14,13 +16,17 @@ const Brewery = (props) => {
                     <h1>{props.name}</h1>
                 </div>
                 <div className="brewery-data">
-                    <p className="brewery-phoneNumber">{props.phoneNumber}</p>
+                    {/* <p className="brewery-phoneNumber">{props.phoneNumber}</p> */}
                     <p className="brewery-address">{props.address}</p>
-                    {/* <div id="brewery-hours">hours
-                        <ul className="hours-popup">
-                            {listHours}
-                        </ul>
-                    </div> */}
+                    <p className="brewery-rating">{props.rating}</p>
+
+                    {isOpen() === true ? (
+                        <p className="brewery-open">open</p>
+                    ) : (
+                        <p className="brewery-closed">closed</p>
+                    )}
+
+                    <a href={props.website} className="brewery-website">Visit website</a>
                     <p className="social-media-icons">
                         <a href="#" className="fa fa-facebook"></a>
                         <a href={props.instagram} className="fa fa-instagram"></a>
